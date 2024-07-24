12630d26ea1e01030f84a4f40c5cdf7f439789ac
hiquecolombo <hikcolombo9@gmail.com>
2024-07-24 19:27:44 -0300

    COMMIT INICIAL PROJETO LOJA VIRTUAL

---
+++ src/main/java/hique/dev/lojavirtual/LojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package hique.dev.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+}
