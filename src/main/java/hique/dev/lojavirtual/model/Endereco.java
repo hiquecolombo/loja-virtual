21070a296d260ec40a684c68d6056f702ffd865a
hiquecolombo <hikcolombo9@gmail.com>
2024-07-25 19:28:48 -0300

    Primeiras configurações de segurança

--- src/main/java/hique/dev/lojavirtual/model/Endereco.java
+++ src/main/java/hique/dev/lojavirtual/model/Endereco.java
@@ -3,8 +3,11 @@ package hique.dev.lojavirtual.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import hique.dev.lojavirtual.enums.TipoEndereco;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -42,6 +45,17 @@ public class Endereco implements Serializable {
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
+
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco;
+
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
+	}
 
 	public Long getId() {
 		return id;
